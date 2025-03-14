e3e88b71640849938e32fe146eebef934e942604
Kacper Cyrankowski <cyrankowskikacper123@gmail.com>
2025-03-14 10:06:14 +0100

    commit

--- ZeroHeaven/main.cpp
+++ ZeroHeaven/main.cpp
@@ -3,23 +3,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
+#include <GL/glew.h>
 
 static SDL_Window* window = nullptr;
 static SDL_Renderer* renderer = nullptr;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
 {
-	SDL_SetAppMetadata("Test", "1.0", "com.example.test");
-
 	if (!SDL_Init(SDL_INIT_VIDEO))
 	{
 		SDL_Log("Error initializing SDL: %s", SDL_GetError());
-		return SDL_APP_FAILURE;
-	}
-
-	if (!SDL_CreateWindowAndRenderer("examples/renderer/lines", 640, 480, 0, &window, &renderer))
-	{
-		SDL_Log("Error creating window/renderer: %s", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
 
@@ -28,7 +21,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-	if (event->type == SDL_EVENT_QUIT) 
+	if (event->type == SDL_EVENT_QUIT || event->key.key == SDLK_ESCAPE) 
 	{
 		return SDL_APP_SUCCESS; 
 	}
