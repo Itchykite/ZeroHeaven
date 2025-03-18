07eedb37fedfcc4f0118ee4cd06b72638c01e5d9
Kacper Cyrankowski <cyrankowskikacper123@gmail.com>
2025-03-18 08:52:06 +0100

    damn

--- ZeroHeaven/main.cpp
+++ ZeroHeaven/main.cpp
@@ -1,12 +1,15 @@
 #define SDL_MAIN_USE_CALLBACKS
 
+#include <GL/glew.h>
+#include <GL/gl.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
-#include <GL/glew.h>
+#include <SDL3/SDL_opengl.h>
 
 static SDL_Window* window = nullptr;
 static SDL_Renderer* renderer = nullptr;
+static SDL_GLContext glContext = nullptr;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
 {
@@ -15,6 +18,33 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
 		SDL_Log("Error initializing SDL: %s", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
+
+    // Tworzenie okna SDL
+    window = SDL_CreateWindow("OpenGL in SDL", 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) 
+    {
+        SDL_Log("Error creating window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Tworzenie kontekstu OpenGL
+    glContext = SDL_GL_CreateContext(window);
+    if (!glContext) 
+    {
+        SDL_Log("Error creating OpenGL context: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Inicjalizacja GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) 
+    {
+        SDL_Log("Error initializing GLEW");
+        return SDL_APP_FAILURE;
+    }
+
+    // Ustawienia OpenGL (przyk³adowe)
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Ustawienie koloru t³a na czarny
 
 	return SDL_APP_CONTINUE;
 }
